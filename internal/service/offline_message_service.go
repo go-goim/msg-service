b0d8ce904796a28851a8cfe625daf99841bd3df7
yusank <yusankurban@gmail.com>
2022-07-11 18:37:15 +0800

    fix: message

--- internal/service/offline_message_service.go
+++ internal/service/offline_message_service.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"strconv"
 
-	"github.com/apache/rocketmq-client-go/v2/primitive"
 	redisv8 "github.com/go-redis/redis/v8"
 
 	responsepb "github.com/go-goim/api/transport/response"
@@ -19,7 +18,7 @@ import (
 )
 
 type OfflineMessageService struct {
-	messagev1.UnimplementedOfflineMessageServer
+	messagev1.UnimplementedOfflineMessageServiceServer
 }
 
 func (o *OfflineMessageService) QueryOfflineMessage(ctx context.Context, req *messagev1.QueryOfflineMessageReq) (
@@ -29,19 +28,11 @@ func (o *OfflineMessageService) QueryOfflineMessage(ctx context.Context, req *me
 	}
 
 	log.Info("req=", req.String())
-	msgID, err := primitive.UnmarshalMsgID([]byte(req.GetLastMsgSeq()))
-	if err != nil {
-		log.Info("unmarshal msg id err=", err)
-		rsp.Response = responsepb.NewBaseResponseWithError(err)
-		return rsp, nil
-	}
-
-	log.Info("unmarshal msg", "host", msgID.Addr, "port", msgID.Port, "offset", msgID.Offset)
 
 	cnt, err := app.GetApplication().Redis.ZCount(ctx,
 		consts.GetUserOfflineQueueKey(req.GetUserId()),
 		// offset add 1 to skip the message user last online msg
-		strconv.FormatInt(msgID.Offset+1, 10),
+		strconv.FormatInt(req.GetLastMsgId()+1, 10),
 		"+inf").Result()
 	if err != nil {
 		rsp.Response = responsepb.NewBaseResponseWithError(err)
@@ -56,7 +47,7 @@ func (o *OfflineMessageService) QueryOfflineMessage(ctx context.Context, req *me
 	results, err := app.GetApplication().Redis.ZRangeByScoreWithScores(ctx,
 		consts.GetUserOfflineQueueKey(req.GetUserId()), &redisv8.ZRangeBy{
 			// offset add 1 to skip the message user last online msg
-			Min:    strconv.FormatInt(msgID.Offset+1, 10),
+			Min:    strconv.FormatInt(req.GetLastMsgId()+1, 10),
 			Max:    "+inf",
 			Offset: int64((req.GetPage() - 1) * req.GetPageSize()),
 			Count:  int64(req.GetPageSize()),
@@ -66,10 +57,10 @@ func (o *OfflineMessageService) QueryOfflineMessage(ctx context.Context, req *me
 		return rsp, nil
 	}
 
-	rsp.Messages = make([]*messagev1.BriefMessage, len(results))
+	rsp.Messages = make([]*messagev1.Message, len(results))
 	for i, result := range results {
 		str := result.Member.(string)
-		msg := new(messagev1.BriefMessage)
+		msg := new(messagev1.Message)
 		if err = json.Unmarshal([]byte(str), msg); err != nil {
 			rsp.Response = responsepb.NewBaseResponseWithError(err)
 			return rsp, nil
